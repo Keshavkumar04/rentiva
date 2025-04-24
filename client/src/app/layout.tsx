c433532462ccfd78fc9943acea78bbeca08e733d
Keshavkumar04 <97456331+Keshavkumar04@users.noreply.github.com>
2025-04-24 08:27:23 +0530

    initial codebase for thr project real-estate

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+import Providers from "./providers";
+import { Toaster } from "@/components/ui/sonner";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+        <Toaster closeButton />
+      </body>
+    </html>
+  );
+}
